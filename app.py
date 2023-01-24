688495aa1dce9d486a5aad486addb9e8aef3e3e0
gwroth89 <gregory.roth.89@gmail.com>
2023-01-23 19:05:40 -0800

    updating app.py with db

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask import render_template
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import jsonify
 
 #Flask Setup
 app = Flask(__name__)
@@ -9,13 +15,19 @@ def welcome():
     """List all available api routes"""
     return(
         f'Available Routes:<br/>'
-        f'/api/v1.0/drought<br/>'
-        f'/api/v1.0/consumption'
+        f'Coming Soon'
     )
 
-@app.route('/data')
+@app.route('/data', methods='GET')
 def data ():
+    base = automap_base()
+    engine = create_engine("sqlite:///database/drought_water.db")
+    base.prepare(autoload_with=engine)
+    drought = base.drought_water.drought
+    water = base.drought_water.water
+    session = Session(engine)
     return jsonify(data)
+    session.close()
 
 if __name__ == '__main__':
     app.run()
