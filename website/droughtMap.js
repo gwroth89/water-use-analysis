8866c1c1af4e9a66769d333c329a43055af59497
Kpfloogs <kmpflugi@gmail.com>
2023-01-30 10:37:16 -0600

    cleaning up code

--- website/droughtMap.js
+++ website/droughtMap.js
@@ -1,29 +1,36 @@
+
+//Connecting our Leaflet map to the html on "map"
 let myMap = L.map("map", {
   center: [39.8283, -98.5795],
   zoom: 4
   });
   
+  //bringing designating our basemap layer
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
   
-  // Load the GeoJSON data. this is the county data, we'll need this in our data to pull from flask?
+  // Loading the GeoJSON data from our flask render as well as available polygon type county data
   
   let Counties = "https://water-use-analysis.onrender.com/average";
   let geoData = "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json";
   
+  //creating a letible for the choropleth layer
   let geojson;
   
-  
-  
+//Retrieving the data with d3.js
   d3.json(Counties).then(function(county) {
     d3.json(geoData).then(function(geo) {
+      //filtering the geo data to look for specific key:object pairs
       geo.features = geo.features.filter(feature => feature.properties)
     
-  
+  //calling the choropleth layer
       geojson = L.choropleth(geo, {
         valueProperty: function(feature){
+          //filtering our data to match the FIPS key in the geo data to get polygon coordinates
         val = county.result.filter(counti => counti.Fips == feature.id);
+          //looking through the data and pulling in the key "Total withdrawals total", if not found,
+      //returning the unmatched FIPS in the console log
           if(val[0]) {
             return val[0]["Abnormally dry"]; 
           } else { 
@@ -31,6 +38,7 @@ let myMap = L.map("map", {
                 return 0
               }
       },
+      //styling the choropleth layer
       scale: ["#ffffb2", "#b10026"],
       steps: 10,
       mode: "q",
@@ -41,6 +49,7 @@ let myMap = L.map("map", {
         fillOpacity: 0.8,
       },
   
+      //creating the action events to highlight the county polygon when hovered over by mouse
       onEachFeature: function(feature, layer) {
         layer.on({
           mouseover: function highlightFeature(event) {
@@ -55,35 +64,39 @@ let myMap = L.map("map", {
             // layer.bringToFront();
             info.update(layer.feature.properties);
           },
+          //resetting the polygon to not be highlighted after mouse has passed
           mouseout: function resetHighlight(event) {
             geojson.resetStyle(event.target);
             info.update();
             },
             
         });
-        
+        //setting markers with info from the data API
         layer.bindPopup("<strong>" + feature.properties.NAME + "</strong><br /><br />Percentage of county in Moderate Drought " + 
           Math.round(val.map(counti => counti["Abnormally dry"]) + '%'));
       }
     
       }).addTo(myMap)
-
+    //creating the legend
+    //calling out the legend position
       let legend = L.control({ position: "bottomleft" });
     legend.onAdd = function() {
+      //creating a div in the html for the legend and creating letibles for the style
       let div = L.DomUtil.create("div", "info legend");
       let limits = geojson.options.limits;
       let colors = geojson.options.colors;
       let labels = [];
   
-      // Add the minimum and maximum.
+      // Adding the minimum and maximum measurements to display on the legend
       let legendInfo = "<h5>Percentage of County in Exceptional Drought</h5>" +
         "<div class=\"labels\">" +
           "<div class=\"min\">" + limits[0] + "</div>" +
           "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
         "</div>";
-  
+      
+      //appending the div with the legend info
       div.innerHTML = legendInfo;
-  
+      //setting the colors for the legend
       limits.forEach(function(limit, index) {
         labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
       });
--- website/waterMap.js
+++ website/waterMap.js
@@ -1,29 +1,35 @@
+
+//Connecting our Leaflet map to the html on "map"
 let myMap = L.map("map", {
   center: [39.8283, -98.5795],
   zoom: 4
   });
 
+//bringing designating our basemap layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Load the GeoJSON data. this is the county data, we'll need this in our data to pull from flask?
-
+// Loading the GeoJSON data from our flask render as well as available polygon type county data
 let water = "https://water-use-analysis.onrender.com/water";
 let geoData = "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json";
 
+//creating a letible for the choropleth layer
 let geojson;
-let geojson2;
 
-
+//Retrieving the data with d3.js
 d3.json(water).then(function(county) {
   d3.json(geoData).then(function(geo) {
+    //filtering the geo data to look for specific key:object pairs
     geo.features = geo.features.filter(feature => feature.properties);
     
-  
+  //calling the choropleth layer 
     geojson = L.choropleth(geo, {
       valueProperty: function(feature){
+      //filtering our data to match the FIPS key in the geo data to get polygon coordinates
       val = county.result.filter(counti => counti.Fips == feature.id);
+      //looking through the data and pulling in the key "Total withdrawals total", if not found,
+      //returning the unmatched FIPS in the console log
       if(val[0]) {
         return val[0]["Total withdrawals total"]; 
       } else { 
@@ -32,6 +38,7 @@ d3.json(water).then(function(county) {
           }
 
     },
+    //styling the choropleth layer
     scale: ["#95e1e9", "#3d54c8"],
     steps: 10,
     mode: "q",
@@ -42,49 +49,55 @@ d3.json(water).then(function(county) {
       fillOpacity: .8,
     },
 
+    //creating the action events to highlight the county polygon when hovered over by mouse
     onEachFeature: function(feature, layer) {
       layer.on({
         mouseover: function highlightFeature(event) {
           let layer = event.target;
           layer.setStyle({
-              weight: 5,
+              weight: 3,
               color: 'black',
               dashArray: '',
-              fillOpactity: .9
+              fillOpactity: .8
           });
 
           // layer.bringToFront();
           info.update(layer.feature.properties);
         },
+        //resetting the polygon to not be highlighted after mouse has passed
         mouseout: function resetHighlight(event) {
           geojson.resetStyle(event.target);
           info.update();
           },
           
       });
-      
+      //setting markers with info from the data API
       layer.bindPopup("<strong>" + feature.properties.NAME + "</strong><br /><br />Total water withdrawals in Mgal/d: " + 
       val.map(counti => counti["Total withdrawals total"]));
     }
   
     }).addTo(myMap)
 
+    //creating the legend
+    //calling out the legend position
     let legend = L.control({ position: "bottomleft" });
+    //creating a div in the html for the legend and creating letibles for the style
     legend.onAdd = function() {
       let div = L.DomUtil.create("div", "info legend");
       let limits = geojson.options.limits;
       let colors = geojson.options.colors;
       let labels = [];
   
-      // Add the minimum and maximum.
+      // Adding the minimum and maximum measurements to display on the legend
       let legendInfo = "<h5>Total Water Usage by County in Mgal/d </h5>" +
         "<div class=\"labels\">" +
           "<div class=\"min\">" + limits[0] + "</div>" +
           "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
         "</div>";
-  
+      
+      //appending the div with the legend info
       div.innerHTML = legendInfo;
-  
+      //setting the colors for the legend
       limits.forEach(function(limit, index) {
         labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
       });
